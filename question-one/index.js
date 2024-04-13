6558706a287263299c4756b0f1b47fd25614ef01
Roland-Sankara <roland1sankara@gmail.com>
2024-04-13 17:57:07 +0300

    add solution for questions one and two

---
+++ question-one/index.js
@@ -0,0 +1,91 @@
+// alert("We are doing well")
+
+// prompt("What is you name?")
+// prompt("What is you name?")
+// prompt("What is you name?")
+// prompt("What is you name?")
+// prompt("What is you name?")
+
+
+// Data Types
+
+/***
+ * Number
+ * String
+ * Boolean
+ * Null
+ * Undefined
+ * 
+ * BigInt
+ * Symbol
+ */
+
+
+
+// Values and Variables
+
+// 19
+// -30
+// 12.5
+
+// "String"
+// true
+// false
+
+// null
+// undefined
+
+
+// Using Variables
+// let username = "Kevin";
+// // the value of this variable can be changed from time to time
+
+// const PI = 3.14;
+// // The value should not be altered
+
+// username = "Roland"
+// // JS Console in the DEV TOOLs
+// // console.log(PI)
+
+
+
+// Functions 
+
+// function greetPerson(){
+//     // code block
+// }
+
+// // Arrow Function
+// const greetMe = ()=>{
+
+// }
+
+// function greetUser(name){
+//     console.log(`Hello ${name}..., happy to see you`)
+// }
+
+// greetUser("Kay Willis")
+// greetUser("Clinton")
+
+
+// let myName = "Roland";
+
+// myName === "Myron"
+// // false
+
+// myName === "Roland"
+// // true
+
+
+let age = prompt("How old are you?")
+
+if(age >= 18){
+    console.log("You qualify for a USA visa")
+}else{
+    console.log("You are under age")
+}
+
+
+
+
+
